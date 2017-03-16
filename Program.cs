d541d32af3bd3ec3174cb1ec4906222fbc9cf3bb
Neldris <richieking1978@gmail.com>
2017-03-16 15:28:41 -0700

    Main file

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Maps.Geocoding;
+using System.Net.Http;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using AmplifiConsoleApp.View;
+using AmplifiConsoleApp.Geo.GeoActivity;
+using AmplifiConsoleApp.Geo;
+using System.IO;
+
+namespace AmplifiConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string [ ] args) {
+            AmplifiResultsViewer view = new AmplifiResultsViewer();
+
+            //Get File path 
+            string fPath = "Use auto";
+            if (args.Length >1)
+                fPath = args [ 0 ];
+            view.Start(Path.GetDirectoryName(fPath)); //set path
+           
+           
+        }
+        
+    }
+}
